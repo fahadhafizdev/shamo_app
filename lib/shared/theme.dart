237e8c63bd42ed56d8607aec9ad8beac1b1d141f
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-02 17:29:46 +0700

    create theme, splash_page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color mainColor = Color(0xff1F1D2B);
+Color purpleColor = Color(0xff6C5ECF);
+Color blackColor = Color(0xff504F5E);
+Color whiteColor = Color(0xffF1F0F2);
+Color blueColor = Color(0xff38ABBE);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;

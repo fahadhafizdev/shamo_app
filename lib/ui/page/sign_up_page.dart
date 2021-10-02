eb708c369c3476f420347f08ccc383a8e369ea80
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-02 19:45:18 +0700

    Create sign_up_page.dart

---
+++ lib/ui/page/sign_up_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/shared/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

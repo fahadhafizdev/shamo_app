c92caae6ed0d3ff67a197368a03ff6abedfe0a6a
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-03 17:34:28 +0700

    Create home_page.dart

---
+++ lib/ui/page/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/shared/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}

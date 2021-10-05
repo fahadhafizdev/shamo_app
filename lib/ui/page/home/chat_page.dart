5a569feb7a2bfbe47442c51df3c08696acfd7223
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-05 20:29:20 +0700

    create homepage, profilepage, wishlistpage, chatpage

---
+++ lib/ui/page/home/chat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/shared/theme.dart';
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Chat Page'),
+    );
+  }
+}
---
+++ lib/ui/page/home/wishlist_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/shared/theme.dart';
+
+class WishListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('WishList Page'),
+    );
+  }
+}

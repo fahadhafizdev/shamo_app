b422bfd38bd27c3682baceecd27c4e2cc04463a5
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-07 18:20:04 +0700

    Update wishlist_page.dart

--- lib/ui/page/home/wishlist_page.dart
+++ lib/ui/page/home/wishlist_page.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:shamo_app/shared/theme.dart';
+import 'package:shamo_app/ui/widget/custom_header_widget.dart';
 
 class WishListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'WishList Page',
-        style: whiteTextStyle,
-      ),
+    Widget contentNoMessage() {
+      return Center(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20, top: 200),
+              child: Image.asset(
+                'assets/images/icon_love.png',
+                color: blueColor,
+                height: 80,
+                width: 80,
+              ),
+            ),
+            Text(
+              ' You don\'t have dream shoes?',
+              style: whiteTextStyle.copyWith(
+                fontWeight: medium,
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: blackTextStyle,
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 44,
+              width: 152,
+              child: ElevatedButton(
+                style: btnStyle,
+                onPressed: () {},
+                child: Text(
+                  'Explore Store',
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        CustomHeader(text: 'Favorite Shoes'),
+        contentNoMessage(),
+      ],
     );
   }
 }

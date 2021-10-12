975ef737791086b1319feee9a65baef6b9de1bf1
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-12 17:39:08 +0700

    Update cart_page.dart
    contentNoCart done

--- lib/ui/page/cart_page.dart
+++ lib/ui/page/cart_page.dart
@@ -26,12 +26,68 @@ class CartPage extends StatelessWidget {
       );
     }
 
+    Widget contentNoCart() {
+      return Center(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 20,
+                top: 180,
+              ),
+              height: 69,
+              width: 79,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/icon_cart.png',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 12),
+              child: Text(
+                'Opss! Your Cart is Empty',
+                textAlign: TextAlign.center,
+                style: whiteTextStyle.copyWith(
+                  fontWeight: medium,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              textAlign: TextAlign.center,
+              style: blackTextStyle.copyWith(
+                color: Color(0xff999999),
+              ),
+            ),
+            Container(
+              height: 44,
+              width: MediaQuery.of(context).size.width - (112 + 111),
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: ElevatedButton(
+                style: btnStyle,
+                onPressed: () {},
+                child: Text(
+                  'Explore Store',
+                  style: whiteTextStyle.copyWith(
+                    fontWeight: medium,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: header(),
       backgroundColor: bgColor2,
-      body: Center(
-        child: Text('Cart Page'),
-      ),
+      body: contentNoCart(),
     );
   }
 }

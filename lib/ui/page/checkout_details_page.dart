c112668cc261aede17d292af36d9aa85dfd8c1fc
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-13 22:41:22 +0700

    Update checkout_details_page.dart

--- lib/ui/page/checkout_details_page.dart
+++ lib/ui/page/checkout_details_page.dart
@@ -57,10 +57,97 @@ class CheckoutDetailsPage extends StatelessWidget {
       );
     }
 
+    Widget listItem() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: 30,
+              left: 30,
+              bottom: 12,
+              right: 30,
+            ),
+            child: Text(
+              'List Items',
+              style: whiteTextStyle.copyWith(
+                fontWeight: medium,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            padding: EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 12,
+            ),
+            decoration: BoxDecoration(
+              color: bgColor4,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 12),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/shoes1.png'),
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                //NOTE: TEXTS
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Terrex Urban Low',
+                        overflow: TextOverflow.clip,
+                        style: whiteTextStyle.copyWith(fontWeight: semiBold),
+                      ),
+                      Text(
+                        '\$143,98',
+                        overflow: TextOverflow.clip,
+                        style: blueTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: Text(
+                    '2 Items',
+                    style: blackTextStyle.copyWith(
+                      color: greyColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgColor2,
       bottomNavigationBar: customBottomnNav(),
       appBar: header(),
+      body: ListView(
+        children: [
+          listItem(),
+        ],
+      ),
     );
   }
 }

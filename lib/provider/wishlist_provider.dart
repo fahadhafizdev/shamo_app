a3c8481cb2d9bc5173e2b596ae1933176730c28e
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-21 21:02:44 +0700

    Update wishlist_provider.dart

--- lib/provider/wishlist_provider.dart
+++ lib/provider/wishlist_provider.dart
@@ -1,3 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/models/product_model.dart';
 
-class WishListProvider with ChangeNotifier {}
+class WishListProvider with ChangeNotifier {
+  List<ProductModel> _wishList = [];
+
+  List<ProductModel> get wishList => _wishList;
+
+  set wishList(List<ProductModel> newWishList) {
+    _wishList = newWishList;
+    notifyListeners();
+  }
+
+  setProducts(ProductModel product) {
+    if (isWishList(product)) {
+      _wishList.removeWhere((element) => element.id == product.id);
+    } else {
+      _wishList.add(product);
+    }
+
+    notifyListeners();
+  }
+
+  isWishList(ProductModel product) {
+    if (_wishList.indexWhere((element) => element.id == product.id) == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}

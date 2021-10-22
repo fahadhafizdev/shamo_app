b5c43d4ac7a939182e9f3d9824406d203bfdff67
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-22 12:26:25 +0700

    Update custom_wishlist_widget.dart

--- lib/ui/widget/custom_wishlist_widget.dart
+++ lib/ui/widget/custom_wishlist_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shamo_app/models/product_model.dart';
+import 'package:shamo_app/provider/wishlist_provider.dart';
 import 'package:shamo_app/shared/theme.dart';
+import 'package:shamo_app/ui/page/detail_product_page.dart';
 
 class CustomWishList extends StatelessWidget {
   final ProductModel products;
@@ -8,53 +11,69 @@ class CustomWishList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.only(
-        left: defaultMargin,
-        right: defaultMargin,
-        bottom: 20,
-      ),
-      padding: EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 12,
-      ),
-      decoration: BoxDecoration(
-        color: bgColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.only(right: 12),
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              image: DecorationImage(
-                image: NetworkImage(products.galleries[0].url),
+    WishListProvider wishListProvider = Provider.of<WishListProvider>(context);
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailProductPage(products),
+          ),
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: 20,
+        ),
+        padding: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 12,
+        ),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: 12),
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: NetworkImage(products.galleries[0].url),
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  products.name,
-                  overflow: TextOverflow.clip,
-                  style: whiteTextStyle.copyWith(fontWeight: semiBold),
-                ),
-                Text('\$${products.price}', style: blueTextStyle),
-              ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    products.name,
+                    overflow: TextOverflow.clip,
+                    style: whiteTextStyle.copyWith(fontWeight: semiBold),
+                  ),
+                  Text('\$${products.price}', style: blueTextStyle),
+                ],
+              ),
             ),
-          ),
-          Image.asset(
-            'assets/images/icon_love_round_actived.png',
-            height: 34,
-          ),
-        ],
+            GestureDetector(
+              onTap: () {
+                wishListProvider.setProducts(products);
+              },
+              child: Image.asset(
+                'assets/images/icon_love_round_actived.png',
+                height: 34,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

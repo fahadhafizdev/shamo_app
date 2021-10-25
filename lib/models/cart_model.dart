e3137ab40f627b91d6470011b727047d719a6184
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-25 11:49:27 +0700

    create cart model

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,27 @@
+import 'package:shamo_app/models/product_model.dart';
+
+class CartModel {
+  int id;
+  ProductModel product;
+  int quantity;
+
+  CartModel({this.id, this.product, this.quantity});
+
+  CartModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    product = json['product'];
+    quantity = json['quantity'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'product': product.toJson(),
+      'quantity': quantity,
+    };
+  }
+
+  double getTotalPrice() {
+    return product.price * quantity;
+  }
+}
--- lib/ui/widget/custom_new_product_widget.dart
+++ lib/ui/widget/custom_new_product_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shamo_app/models/product_model.dart';
 import 'package:shamo_app/shared/theme.dart';
+import 'package:shamo_app/ui/page/detail_product_page.dart';
 
 class CustomNewProduct extends StatelessWidget {
   final ProductModel products;
@@ -8,50 +9,60 @@ class CustomNewProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      width: 290,
-      margin: EdgeInsets.only(bottom: defaultMargin),
-      child: Row(
-        children: [
-          Container(
-            height: 120,
-            width: 120,
-            margin: EdgeInsets.only(right: 12),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: whiteColor,
-              image: DecorationImage(
-                image: NetworkImage(products.galleries[0].url),
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailProductPage(products),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                products.category.name,
-                style: greyTextStyle.copyWith(fontSize: 12),
-              ),
-              Container(
-                width: 151,
-                child: Text(
-                  products.name,
-                  style: whiteTextStyle.copyWith(
-                    fontWeight: semiBold,
-                    fontSize: 16,
-                  ),
-                  overflow: TextOverflow.clip,
+        );
+      },
+      child: Container(
+        height: 120,
+        width: 290,
+        margin: EdgeInsets.only(bottom: defaultMargin),
+        child: Row(
+          children: [
+            Container(
+              height: 120,
+              width: 120,
+              margin: EdgeInsets.only(right: 12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: whiteColor,
+                image: DecorationImage(
+                  image: NetworkImage(products.galleries[0].url),
                 ),
               ),
-              Text(
-                '\$${products.price}',
-                style: blueTextStyle.copyWith(fontWeight: medium),
-              )
-            ],
-          ),
-        ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  products.category.name,
+                  style: greyTextStyle.copyWith(fontSize: 12),
+                ),
+                Container(
+                  width: 151,
+                  child: Text(
+                    products.name,
+                    style: whiteTextStyle.copyWith(
+                      fontWeight: semiBold,
+                      fontSize: 16,
+                    ),
+                    overflow: TextOverflow.clip,
+                  ),
+                ),
+                Text(
+                  '\$${products.price}',
+                  style: blueTextStyle.copyWith(fontWeight: medium),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

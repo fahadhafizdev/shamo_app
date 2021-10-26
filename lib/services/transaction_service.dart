fb1fe5668373ef437078afad318676c3dfd79d37
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-10-26 16:22:41 +0700

    Update transaction_service.dart

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,4 +1,43 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shamo_app/models/cart_model.dart';
 
-class TransactionService {}
+class TransactionService {
+  String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
+
+  Future checkout(
+      String token, List<CartModel> carts, double totalPrice) async {
+    var url = '$baseUrl/checkout';
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    };
+    var body = jsonEncode({
+      'address': 'Marsemoon',
+      'items': carts
+          .map((cart) => {
+                'id': cart.product.id,
+                'quantity': cart.quantity,
+              })
+          .toList(),
+      'status': "PENDING",
+      'total_price': totalPrice,
+      'shipping_price': 0,
+    });
+
+    var response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+
+    print('statusconde : ${response.statusCode}');
+    print('body : ${response.body}');
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception('Gagal Transaksi');
+    }
+  }
+}

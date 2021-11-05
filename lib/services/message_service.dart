83acd4afbb1986016443f3220ff9f388b56e1f83
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-11-05 19:10:45 +0700

    Update message_service.dart

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -9,5 +9,22 @@ class MessageService {
       {UserModel user,
       bool isFromUser,
       String message,
-      ProductModel product}) async {}
+      ProductModel product}) async {
+    try {
+      firestore.collection('messages').add({
+        'userId': user.id,
+        'userName': user.username,
+        'userImage': user.photoUrl,
+        'isFromUser': true,
+        'message': message,
+        'product': product is UninitializedProductModel ? {} : product.toJson(),
+        'createdAt': DateTime.now().toString(),
+        'updatedAt': DateTime.now().toString(),
+      }).then(
+        (value) => print('pesan berhasil dikirim'),
+      );
+    } catch (e) {
+      throw Exception('Pesan gagal dikirim');
+    }
+  }
 }

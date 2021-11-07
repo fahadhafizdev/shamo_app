4e9f2ef973a4a2e919240731760fbcd30a29dafa
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-11-07 16:45:32 +0700

    Update message_service.dart

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,9 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shamo_app/models/message_model.dart';
 import 'package:shamo_app/models/product_model.dart';
 import 'package:shamo_app/models/user_model.dart';
 
 class MessageService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<List<MessageModel>> getMessageByUserId({int userId}) {
+    try {
+      return firestore
+          .collection('messages')
+          .where('userId', isEqualTo: userId)
+          .snapshots()
+          .map((QuerySnapshot list) {
+        var result = list.docs.map<MessageModel>((DocumentSnapshot message) {
+          print(message.data());
+          return MessageModel.fromJson(message.data());
+        }).toList();
+
+        //Note: method untuk sorting
+        result.sort(
+          (MessageModel a, MessageModel b) =>
+              a.createdAt.compareTo(b.createdAt),
+        );
+
+        return result;
+      });
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 
   Future<void> addMessage(
       {UserModel user,
